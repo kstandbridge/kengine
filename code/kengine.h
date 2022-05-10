0175f23559e43015c13284c6571f814969950058
kstandbridge <prepotency@gmail.com>
2022-05-10 18:23:00 +0100

    chore: todo list

---
+++ code/kengine.h
@@ -0,0 +1,31 @@
+#ifndef KENGINE_H
+
+/* TODO(kstandbridge): 
+
+- Format string unit tests signed decimal integer
+- Format string unit tests string of characters
+- Format string %	A % followed by another % character will write a single % to the stream.
+- Format string u	Unsigned decimal integer
+- Format string o	Unsigned octal
+- Format string x	Unsigned hexadecimal integer
+- Format string X	Unsigned hexadecimal integer (uppercase)
+- Format string f	Decimal floating point, lowercase
+- Format string F	Decimal floating point, uppercase
+- Format string e	Scientific notation (mantissa/exponent), lowercase
+- Format string E	Scientific notation (mantissa/exponent), uppercase
+- Format string g	Use the shortest representation: %e or %f
+- Format string G	Use the shortest representation: %E or %F
+- Format string a	Hexadecimal floating point, lowercase
+- Format string A	Hexadecimal floating point, uppercase
+- Format string c	Character
+- Format string p	Pointer address
+- Format string n	Nothing printed.
+- Format string flags
+- Format string width
+- Format string precision
+
+
+*/
+
+#define KENGINE_H
+#endif //KENGINE_H
