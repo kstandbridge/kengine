bcc455b26a1126a62c93f85319837b8a0f41842f
kstandbridge <prepotency@gmail.com>
2023-04-20 16:34:45 +0100

    refactor: macro renaming due to conflicts

--- kengine/kengine_sha512.c
+++ kengine/kengine_sha512.c
@@ -83,12 +83,12 @@ global u64 GlobalKArray[80] = {
 
 #define Ch(x,y,z)       (z ^ (x & (y ^ z)))
 #define Maj(x,y,z)      (((x | y) & z) | (x & y)) 
-#define S(x, n)         ROR64c(x, n)
+#define S_(x, n)         ROR64c(x, n)
 #define R(x, n)         (((x) &(0xFFFFFFFFFFFFFFFF))>>((u64)n))
-#define Sigma0(x)       (S(x, 28) ^ S(x, 34) ^ S(x, 39))
-#define Sigma1(x)       (S(x, 14) ^ S(x, 18) ^ S(x, 41))
-#define Gamma0(x)       (S(x, 1) ^ S(x, 8) ^ R(x, 7))
-#define Gamma1(x)       (S(x, 19) ^ S(x, 61) ^ R(x, 6))
+#define Sigma0(x)       (S_(x, 28) ^ S_(x, 34) ^ S_(x, 39))
+#define Sigma1(x)       (S_(x, 14) ^ S_(x, 18) ^ S_(x, 41))
+#define Gamma0(x)       (S_(x, 1) ^ S_(x, 8) ^ R(x, 7))
+#define Gamma1(x)       (S_(x, 19) ^ S_(x, 61) ^ R(x, 6))
 #ifndef MIN
 #define MIN(x, y) ( ((x)<(y))?(x):(y) )
 #endif
