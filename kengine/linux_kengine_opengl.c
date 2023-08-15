536d74f962d6e79a10512f83f6592b1e169c2bb5
kstandbridge <prepotency@gmail.com>
2023-08-15 11:00:50 +0100

    002a - Opening a Linux Window

--- kengine/linux_kengine_opengl.c
+++ kengine/linux_kengine_opengl.c
@@ -1,3 +1,56 @@
+internal void
+LinuxProcessPendingMessages(Display *Display, Window Window, Atom WmDeleteWindow)
+{
+    while(XPending(Display))
+    {
+        XEvent Event;
+        XNextEvent(Display, &Event);
+        switch(Event.type)
+        {
+            case ConfigureNotify:
+            {
+                u32 Width = (Event.xconfigure.width >= 0) ? Event.xconfigure.width : 0;
+                u32 Height = (Event.xconfigure.height >= 0) ? Event.xconfigure.height : 0;
+                PlatformConsoleOut("SIZE: %u / %u\n", Width, Height);
+            } break;
+            
+            case DestroyNotify:
+            {
+                if ((Display == Event.xdestroywindow.display) &&
+                    (Window == Event.xdestroywindow.window))
+                {
+                    PlatformConsoleOut("Destroy\n");
+                }
+            } break;
+            
+            case ClientMessage:
+            {
+                if ((Atom)Event.xclient.data.l[0] == WmDeleteWindow)
+                {
+                    PlatformConsoleOut("Delete window\n");
+                }
+            } break;
+
+            case ButtonPress:
+            {
+                XCloseDisplay(Display);
+                break;
+            }
+            
+            case MotionNotify:
+            {
+                f32 X = (f32)Event.xmotion.x;
+                f32 Y = (f32)Event.xmotion.y;
+
+                PlatformConsoleOut("Mouse at %f / %f\n", X, Y);
+            } break;
+
+            default:
+            break;
+        }
+    }
+}
+
 int
 main(int argc, char **argv)
 {
@@ -8,22 +61,18 @@ main(int argc, char **argv)
     Window Window = XCreateSimpleWindow(Display, DefaultRootWindow(Display),
                                         0, 0, 100, 100, 0, White, Black);
 
-    XSelectInput(Display, Window, ButtonPressMask | StructureNotifyMask);
+    XSelectInput(Display, Window, ExposureMask | StructureNotifyMask |
+                 KeyReleaseMask | PointerMotionMask | ButtonPressMask |
+                 ButtonReleaseMask);
 
     XMapWindow(Display, Window);
 
+    Atom WmDeleteWindow = XInternAtom(Display, "WM_DELETE_WINDOW", False);
+    XSetWMProtocols(Display, Window, &WmDeleteWindow, 1);
+
     for(;;)
     {
-        XEvent Event;
-        XNextEvent(Display, &Event);
-        switch(Event.type)
-        {
-            case ButtonPress:
-            {
-                XCloseDisplay(Display);
-                break;
-            }
-        }
+        LinuxProcessPendingMessages(Display, Window, WmDeleteWindow);
     }
 
     return 0;
